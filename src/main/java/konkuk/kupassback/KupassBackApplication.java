5c8d9278c3a35703321f28fa2efa8cd9c8abd229
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-03 23:44:17 +0900

    add: main file

---
+++ src/main/java/konkuk/kupassback/KupassBackApplication.java
@@ -0,0 +1,13 @@
+package konkuk.kupassback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KupassBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KupassBackApplication.class, args);
+	}
+
+}
