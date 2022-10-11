964b1e3404119e4f4369da0a6c10938e53aa25b1
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-11 21:15:17 +0900

    feat: 검색 컨트롤러

---
+++ src/main/java/konkuk/kupassback/controller/ArticleController.java
@@ -0,0 +1,36 @@
+package konkuk.kupassback.controller;
+
+import konkuk.kupassback.dto.ArticleDTO;
+import konkuk.kupassback.dto.ResponseDTO;
+import konkuk.kupassback.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search/article")
+@RequiredArgsConstructor
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO> search(
+            @RequestParam(value = "publisher", required = false, defaultValue = "") String publisher,
+            @RequestParam(value = "category", required = false, defaultValue = "") String category,
+            @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+            @RequestParam(value = "page", required = false, defaultValue = "0") int page
+            ) {
+        PageRequest pageRequest = PageRequest.of(page, 20);
+        List<ArticleDTO> articles = articleService.searchArticle(publisher, keyword, category, pageRequest);
+
+        return new ResponseEntity<>(new ResponseDTO("success", "ok", articles), HttpStatus.OK);
+    }
+}
