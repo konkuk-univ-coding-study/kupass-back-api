d89d02d01fbe406225b824b1b0c60676d157f1b2
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-11 21:16:06 +0900

    add: initial test file

---
+++ src/test/java/konkuk/kupassback/KupassBackApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.kupassback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KupassBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
