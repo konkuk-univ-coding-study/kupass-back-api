7d289bee714b3ab086559a2c0af2cc2362221766
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-11 21:13:00 +0900

    feat: 검색 specification

--- src/main/java/konkuk/kupassback/specification/ArticleSpecification.java
+++ src/main/java/konkuk/kupassback/specification/ArticleSpecification.java
@@ -3,6 +3,8 @@ package konkuk.kupassback.specification;
 import konkuk.kupassback.domain.Article;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +15,13 @@ public class ArticleSpecification {
         return ((root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
             for (Map.Entry<String, Object> keyVal : searchKeys.entrySet()) {
-                predicates.add(criteriaBuilder.equal(root.get(keyVal.getKey()), keyVal.getValue()));
+                if (keyVal.getKey().equals("keyword")) {
+                    Join<Object, Object> join = root.join("keywords", JoinType.INNER);
+                    predicates.add(criteriaBuilder.equal(join.get(keyVal.getKey()), keyVal.getValue()));
+                }
+                else {
+                    predicates.add(criteriaBuilder.equal(root.get(keyVal.getKey()), keyVal.getValue()));
+                }
             }
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         });
