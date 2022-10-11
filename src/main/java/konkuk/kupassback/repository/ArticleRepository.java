8811d3838df5f93e315e2842090824d0184084df
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-11 21:14:32 +0900

    feat: 페이징 기능을 위해 PagingAndSortingRepository로 교체

--- src/main/java/konkuk/kupassback/repository/ArticleRepository.java
+++ src/main/java/konkuk/kupassback/repository/ArticleRepository.java
@@ -1,11 +1,11 @@
 package konkuk.kupassback.repository;
 
 import konkuk.kupassback.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
+public interface ArticleRepository extends PagingAndSortingRepository<Article, Long>, JpaSpecificationExecutor<Article> {
 
 }
