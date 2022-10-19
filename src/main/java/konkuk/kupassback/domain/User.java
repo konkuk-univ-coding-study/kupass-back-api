d0a0c594c8c921bc30968b75ae94ea16dcf0ed78
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-19 22:57:25 +0900

    feat: user 도메인

---
+++ src/main/java/konkuk/kupassback/domain/User.java
@@ -0,0 +1,55 @@
+package konkuk.kupassback.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String nickname;
+
+    private String password;
+
+    @Column(name = "activated")
+    private boolean isActive;
+
+    public User(String nickname, String password) {
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String userId) {
+        this.nickname = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
