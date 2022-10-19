970a74dbb660126312d29b6e1e3a6e915d208e1e
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-19 22:28:35 +0900

    feat: ArticleKeywords 리포지토리

---
+++ src/main/java/konkuk/kupassback/repository/ArticleKeywordsRepository.java
@@ -0,0 +1,10 @@
+package konkuk.kupassback.repository;
+
+import konkuk.kupassback.domain.Article;
+import konkuk.kupassback.domain.ArticleKeywords;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ArticleKeywordsRepository extends PagingAndSortingRepository<ArticleKeywords, Long>,
+        JpaSpecificationExecutor<ArticleKeywords> {
+}
