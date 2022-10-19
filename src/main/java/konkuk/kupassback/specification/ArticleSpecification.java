abfa93f2b3dfc7fa00a6049aef799c09ffceba5f
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-19 22:32:48 +0900

    feat: 키워드로만 검색 기능 추가

--- src/main/java/konkuk/kupassback/specification/ArticleSpecification.java
+++ src/main/java/konkuk/kupassback/specification/ArticleSpecification.java
@@ -1,29 +1,30 @@
 package konkuk.kupassback.specification;
 
-import konkuk.kupassback.domain.Article;
+import konkuk.kupassback.domain.ArticleKeywords;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Subquery;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ArticleSpecification {
-    public static Specification<Article> searchArticle(Map<String, Object> searchKeys) {
+    public static Specification<ArticleKeywords> searchKeyword(String keyword) {
+        return (root, query, criteriaBuilder) -> {
+            Join<Object, Object> articleKeywords = root.join("keyword", JoinType.INNER);
+            return criteriaBuilder.equal(articleKeywords.get("keyword"), keyword);
+        };
+    }
+    public static Specification<ArticleKeywords> searchArticle(Map<String, Object> searchKeys) {
         return ((root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
             for (Map.Entry<String, Object> keyVal : searchKeys.entrySet()) {
                 if (keyVal.getKey().equals("keyword")) {
-                    Join<Object, Object> articleKeywords = root.join("keywords", JoinType.INNER);
-
-                    //
-                    Subquery<>
-                    //
-
+                    Join<Object, Object> articleKeywords = root.join("keyword", JoinType.INNER);
                     predicates.add(criteriaBuilder.equal(articleKeywords.get(keyVal.getKey()), keyVal.getValue()));
+//                    predicates.add(criteriaBuilder.equal(root.get("keyword"), keyVal.getValue()));
                 }
                 else {
                     predicates.add(criteriaBuilder.equal(root.get(keyVal.getKey()), keyVal.getValue()));
