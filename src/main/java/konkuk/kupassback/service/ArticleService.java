faefd5b79fd2153bdcad5bbf2fcbb95106a5d53c
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-19 22:33:39 +0900

    feat: 기사 검색 서비스

---
+++ src/main/java/konkuk/kupassback/service/ArticleService.java
@@ -0,0 +1,49 @@
+package konkuk.kupassback.service;
+
+import konkuk.kupassback.domain.Article;
+import konkuk.kupassback.domain.ArticleKeywords;
+import konkuk.kupassback.dto.ArticleDTO;
+import konkuk.kupassback.repository.ArticleKeywordsRepository;
+import konkuk.kupassback.repository.ArticleRepository;
+import konkuk.kupassback.specification.ArticleSpecification;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ArticleService {
+
+    private final ArticleKeywordsRepository articleKeywordsRepository;
+    private final ArticleRepository articleRepository;
+
+    public List<ArticleDTO> searchArticle(String publisher, String keyword, String category, Pageable page) {
+        Stream<Article> articleStream;
+        if (!keyword.isEmpty()) {
+            articleStream = articleKeywordsRepository.findAll(ArticleSpecification.searchKeyword(keyword), page)
+                    .stream()
+                    .map(ArticleKeywords::getArticle);
+        } else {
+            articleStream = articleRepository.findAll(page)
+                    .stream();
+        }
+        if (!publisher.isEmpty()) {
+            articleStream = articleStream
+                    .filter(article -> article.getPublisher().equals(publisher));
+        }
+        if (!category.isEmpty()) {
+            articleStream = articleStream
+                    .filter(article -> article.getCategory().equals(category));
+        }
+
+        return articleStream
+                .map(ArticleDTO::new)
+                .collect(Collectors.toList());
+    }
+}
