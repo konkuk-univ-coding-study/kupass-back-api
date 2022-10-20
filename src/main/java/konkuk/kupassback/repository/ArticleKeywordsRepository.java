8a46a36ac2c16998565af22c345e6adbe1f332af
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-20 13:10:30 +0900

    fix: 불필요한 import 제거

--- src/main/java/konkuk/kupassback/repository/ArticleKeywordsRepository.java
+++ src/main/java/konkuk/kupassback/repository/ArticleKeywordsRepository.java
@@ -1,6 +1,5 @@
 package konkuk.kupassback.repository;
 
-import konkuk.kupassback.domain.Article;
 import konkuk.kupassback.domain.ArticleKeywords;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
