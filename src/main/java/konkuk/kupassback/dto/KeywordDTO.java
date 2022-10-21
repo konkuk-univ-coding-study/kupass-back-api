9a5f6f58f88e1efeb47959e49ceb4126fb407729
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-21 12:59:56 +0900

    feat: 클라이언트가 전송하는 키워드 dto

---
+++ src/main/java/konkuk/kupassback/dto/KeywordDTO.java
@@ -0,0 +1,13 @@
+package konkuk.kupassback.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class KeywordDTO {
+
+    private String keyword;
+}
