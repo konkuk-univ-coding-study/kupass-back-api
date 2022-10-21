1e40c7033f8ea1ffe8391d1dfebd262bac6c3b26
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-21 12:56:42 +0900

    feat: 유저 관심사 도메인

---
+++ src/main/java/konkuk/kupassback/domain/InterestingKeyword.java
@@ -0,0 +1,27 @@
+package konkuk.kupassback.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class InterestingKeyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interesting_keyword_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id")
+    private Keyword keyword;
+}
