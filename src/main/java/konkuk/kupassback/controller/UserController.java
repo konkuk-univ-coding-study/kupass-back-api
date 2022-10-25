af7c7b950eb5b99dd94b0020836c10b17d8bacbb
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-25 23:24:29 +0900

    feat: 키워드 저장할 유저와 로그인된 유저 일치 검증

--- src/main/java/konkuk/kupassback/controller/UserController.java
+++ src/main/java/konkuk/kupassback/controller/UserController.java
@@ -6,14 +6,19 @@ import konkuk.kupassback.dto.KeywordDTO;
 import konkuk.kupassback.dto.KeywordResponseDTO;
 import konkuk.kupassback.dto.UserResponseDTO;
 import konkuk.kupassback.exceptions.KeywordExistsException;
+import konkuk.kupassback.jwt.TokenProvider;
 import konkuk.kupassback.service.KeywordService;
 import konkuk.kupassback.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+
+import static konkuk.kupassback.jwt.JWTFilter.AUTHORIZATION_HEADER;
 
 @RestController
 @RequestMapping("/api/user")
@@ -22,6 +27,8 @@ public class UserController {
 
     private final UserService userService;
     private final KeywordService keywordService;
+
+    private final TokenProvider tokenProvider;
 
     @GetMapping("/{nickname}/keywords")
     public ResponseEntity<KeywordResponseDTO> getKeywords(@PathVariable String nickname) {
@@ -34,7 +41,17 @@ public class UserController {
     // todo 키워드 저장
     @PostMapping("/{nickname}/keywords")
     public ResponseEntity<UserResponseDTO> saveKeyword(@PathVariable String nickname,
-                                                       @RequestBody KeywordDTO keywordDTO) {
+                                                       @RequestBody KeywordDTO keywordDTO,
+                                                       HttpServletRequest request) {
+        String header = request.getHeader(AUTHORIZATION_HEADER);
+        String token = header.substring(7);
+        String subject = tokenProvider.getSubject(token);
+
+        if (!subject.equals(nickname)) {
+            return new ResponseEntity<>(
+                    new UserResponseDTO("fail", "user doesn't match"), HttpStatus.OK);
+        }
+
         User user = userService.findUser(nickname);
         try {
             keywordService.saveKeyword(user, keywordDTO.getKeyword());
