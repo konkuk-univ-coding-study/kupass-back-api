4a6793a7bca8d10b7203a8643c62d51a25f1ef2d
kyuhyeongkwon <akcps1tp@naver.com>
2022-11-10 14:18:49 +0900

    fix: cors PUT DELETE 메서드 허용

--- src/main/java/konkuk/kupassback/config/WebConfig.java
+++ src/main/java/konkuk/kupassback/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST");
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
     @Override
